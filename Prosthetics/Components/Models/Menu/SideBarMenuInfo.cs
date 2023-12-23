4ad5c7f536879b8e9bef28fbecb82f19a82a3a1d
juniordevops <lukasz.tomalczyk@outlook.com>
2023-12-23 13:02:16 +0100

    Added side bar

---
+++ Prosthetics/Components/Models/Menu/SideBarMenuInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Prosthetics.Components.Models.Menu
+{
+    public class SideBarMenuInfo
+    {
+        public string? Text { get; set; }
+        public string? Icon { get; set; }
+        public string? PageUrl { get; set; }
+    }
+}
